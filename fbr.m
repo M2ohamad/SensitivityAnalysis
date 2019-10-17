e6996fc414c36fabc5e86f245a52f4f44de08aa9
Mohamad Mohamad <m2ohamad@gmail.com>
2019-10-16 21:18:28 -0500

    Delete fbr.m

--- fbr.m
+++
@@ -1,6 +0,0 @@
-tau = 500 * 50 * 10^(-6);
-t = 0:0.001:(tau);
-v1 = 230*sqrt(2)*cos(100*pi*t);
-t1 = arctan(4/(10*pi))*(1/(100*pi));
-A = sqrt(2)*230*cos(100*pi*t1)*exp(t1/tau);
-v2 = A*exp(-t/tau);
